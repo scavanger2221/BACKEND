0a10d69052342d7462739ba2bb024954c3504e7d
Dwi Atmika <dwi.atmikaw@gmail.com>
2022-03-15 21:26:33 +0800

    update

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('besthostels', 'root', '', {
+const db = new Sequelize('besthost_balibesthostels', 'besthost_balibesthostels', 'TTdC7P#7V^AZ', {
     host : 'localhost',
     dialect : 'mysql'
 });
